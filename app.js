1e7b86337a4ae619c9d1e23cfd87c9568ae25de1
Rubén <alu0100309553@ull.edu.es>
2017-02-23 09:17:55 +0000

    cambiando app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function(pet, res){
   res.sendfile(__dirname + '/gh-pages/index.html');
 });
 
 app.use(express.static(path.resolve('gh-pages')));
 
-app.listen(8080);
+app.listen(app.get('port'));
 
 console.log("Servidor Express escuchando en modo %s", app.settings.env + " por el puerto: 8080");
